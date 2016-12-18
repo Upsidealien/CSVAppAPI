4516b29ff5083ccb3b5a2378b912c8c99a586b53
Thomas Cartwright <TJCARTWRIGHT91@GMAIL.COM>
2016-12-18 21:56:28 +1100

    initial commit

---
+++ AppAPITemplate/FirstMenu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using CsvHelper;
+using Newtonsoft.Json;
+
+namespace AppAPITemplate
+{
+	public class FirstMenu : Menu
+	{
+		public FirstMenu()
+		{
+
+			Title = "API Template";
+			Content = list;
+
+			list.ItemsSource = CallAPI(); //Need to add an "await" into this
+
+			list.ItemTapped += (sender, e) =>
+			{
+				list.SelectedItem = null;
+				//Navigation.PushAsync(new SecondMenu(e.Item as MenuItem));
+				ClickMenuItem(e.Item as MenuItem);
+			};
+		}
+
+		public List<MenuItem> CallAPI()
+		{
+			List<MenuItem> menuList;
+
+			List<string> myStringColumn = new List<string>();
+			using (var fileReader = File.OpenText(inFile))
+			using (var csvResult = new CsvHelper.CsvReader(fileReader))
+			{
+				while (csvResult.Read())
+				{
+					string stringField = csvResult.GetField<string>("Header Name");
+					myStringColumn.Add(stringField);
+				}
+			}
+
+
+			return menuList;
+		}
+
+		public void ClickMenuItem(MenuItem itemClicked)
+		{
+			//Implements menu item click
+			//e.g. Navigation.PushAsync(new SecondMenu(e.Item as string));
+			Navigation.PushAsync(new SecondMenu(itemClicked));
+		}
+
+	}
+}
+
---
+++ iOS/Resources/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace AppAPITemplate.iOS
+{
+	public class EmptyClass
+	{
+		public EmptyClass()
+		{
+		}
+	}
+}
+
